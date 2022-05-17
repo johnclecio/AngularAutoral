a36001b6972267d15d6acbe3d9c85b17b5878d02
johnclecio <johnclecio@icloud.com>
2022-05-17 12:12:58 -0300

    MAC1 1.0

---
+++ MAC1/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+const routes: Routes = [
+  {path: '', component: FooterComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
